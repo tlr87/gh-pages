22b2f7d5223f54929269eb6cb111dafc52e453b2
Tom Revill <tom.revill@gmail.com>
2017-06-27 13:32:48 +1200

    Basic Structure

---
+++ art.js
@@ -0,0 +1,17 @@
+
+  var Colours = ["black", "blue", "yellow", "green", "orange", "red"];
+function boxCol() {
+
+  //var c = Math.floor((Math.random() * 6) + 1);
+//  element.style.backgroundColor = Colours[3];
+//  return document.body.style.backgroundColor = Colours[c];
+//document.body.style.backgroundColor = "red";
+}
+
+//boxCol();
+
+
+
+    function color2() {
+        document.body.style.backgroundColor = 'orange';
+    }
