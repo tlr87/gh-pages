1c41a7e5c30e9c1e0ed5d48b0fbf9aef5a80d642
Tom Revill <tom.revill@gmail.com>
2017-06-27 22:58:57 +1200

    Before removing test code.

--- art.js
+++ art.js
@@ -1,4 +1,19 @@
 var className ="";
+var classBG ="";
+//var loopBool = false;
+
+
+function addDiv() {
+  var div = document.createElement("div");
+  div.innerHTML = "Hello";
+  document.getElementsByClassName("art")[0].appendChild(div).classList.add("box");
+  document.getElementsByClassName("art")[0].appendChild(div).classList.add("b4");
+  loopBool = true;
+  delayTimer(2);
+console.log( BGcol(classBG)); 
+
+//  loopBool = false;
+}
 
 BGcol();
 
@@ -21,9 +36,20 @@ var timer = timeValue*1000;
 setTimeout(function(){ console.log("the death of us all!"); }, timer);
 }
 
+
 for (var i = 1; i < elements.length+1; i++) {
 classBG = "b"+[i];
 BGcol(classBG);
-delayTimer(2);
-
+delayTimer(1+1);
 }
+
+
+
+loopBool=false;
+
+//for (var i = 1; i == i; i++) {
+//classBG = "b"+[i];
+//BGcol(classBG);
+//delayTimer(2);
+//console.log("Hello");
+//}
