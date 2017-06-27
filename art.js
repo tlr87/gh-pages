fe9841b48c168b92cdc6d45e9847b7d7b5d37e4a
Tom Revill <tom.revill@gmail.com>
2017-06-27 18:54:15 +1200

    baseCode Fixed

--- art.js
+++ art.js
@@ -1,35 +1,29 @@
+var className ="";
+
 BGcol();
 
-function BGcol() {
+function BGcol(className) {
   setInterval(function() {
     var r = Math.round(Math.random() * 255);
     var g = Math.round(Math.random() * 255);
     var b = Math.round(Math.random() * 255);
     var bg = "background:rgb(" + r + "," + g + "," + b + ");";
-    var element = document.getElementsByClassName("box")[0];
+    var element = document.getElementsByClassName(className)[0];
     element.style = bg;
-
-
-
-  }, 5000);
-
+  }, 2000);
 }
-
-//var temp = document.getElementsByClassName("art");
-
-//for(i=0; i < temp.length; i++){
-//console.log("hello"[i]);
-//}
-
-
 
 
 var elements = document.getElementsByClassName("art")[0].children;
-//console.log(elements.length+"Hello");
 
-for (var i = 0; i < elements.length; i++) {
-  //doSomething(elements[i]);
-  console.log("hello");
+function delayTimer(timeValue){
+var timer = timeValue*1000;
+setTimeout(function(){ console.log("the death of us all!"); }, timer);
+}
 
+for (var i = 1; i < elements.length+1; i++) {
+classBG = "b"+[i];
+BGcol(classBG);
+delayTimer(2);
 
 }
