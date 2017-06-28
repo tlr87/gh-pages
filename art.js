de5d646ae48a38967934c75a4966ac845aa76c79
Tom Revill <tom.revill@gmail.com>
2017-06-28 21:04:24 +1200

    End?

--- art.js
+++ art.js
@@ -1,22 +1,17 @@
-var className ="";
-var classBG ="";
+var className = "";
+var classBG = "";
 //var loopBool = false;
 var piano = document.getElementById("keys");
 
 function playAudio(Sindex) {
-    var Sarray = ["piano-043.mp3","piano-048.mp3","piano-053.mp3" ,"piano-088.mp3" ];
-    console.log("Play it a again sam!");
-    document.getElementById("keys").setAttribute("src",Sarray[Sindex]);
-    piano.play();
-    delayTimer(4);
+  var Sarray = ["piano-1.mp3", "piano-2.mp3", "piano-3.mp3", "piano-4.mp3",""];
+  console.log("Play it a again sam!");
+  document.getElementById("keys").setAttribute("src", Sarray[Sindex]);
+  piano.play();
+  delayTimer(4);
+  piano.volume = 0.2;
+
 }
-
-
-
-
-
-
-
 
 BGcol();
 
@@ -25,24 +20,29 @@ function BGcol(className) {
     var r = Math.round(Math.random() * 255);
     var g = Math.round(Math.random() * 255);
     var b = Math.round(Math.random() * 255);
+//    var Ivol = Math.random();
     var bg = "background:rgb(" + r + "," + g + "," + b + ");";
     var element = document.getElementsByClassName(className)[0];
     element.style = bg;
-  }, 2000);
+  //  console.log(Ivol);
+  //  Ivol = vol;
+}, 1000);
 }
 
 
 var elements = document.getElementsByClassName("art")[0].children;
 
-function delayTimer(timeValue){
-var timer = timeValue*1000;
-setTimeout(function(){ console.log("the death of us all!"); }, timer);
+function delayTimer(timeValue) {
+  var timer = timeValue * 1000;
+  setTimeout(function() {
+    console.log("the death of us all!");
+  }, timer);
 }
 
 
-for (var i = 1; i < elements.length+1; i++) {
-classBG = "b"+[i];
-BGcol(classBG);
-delayTimer(2);
+for (var i = 1; i < elements.length + 1; i++) {
+  classBG = "b" + [i];
+  BGcol(classBG);
+  delayTimer(2);
 
 }
